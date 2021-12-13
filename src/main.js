72491e085f162d78ec277ca69dc3a1e48328482c
Tom Reinert <tom.reinert@koeln.de>
2021-12-13 11:17:50 +0100

    use alpine data

--- src/main.js
+++ src/main.js
@@ -2,6 +2,42 @@ import Alpine from 'alpinejs'
  
 window.Alpine = Alpine
 
+Alpine.data('app', () => ({
+  swapColors(){
+    let a = Alpine.store('colors').hex1, b = Alpine.store('colors').hex2;
+    [a, b] = [b, a];
+    console.log(`${a} ${b}`);
+    Alpine.store('colors').hex1 = a;
+    Alpine.store('colors').hex2 = b;
+    setHSL(1);
+    setHSL(2);
+    calculateScore();
+  },
+  
+  calculateScore () {
+    let hex1 = Alpine.store('colors').hex1;
+    let hex2 = Alpine.store('colors').hex2;
+    let score = APCAcontrast( sRGBtoY( hex1 ), sRGBtoY( hex2 ) );
+    Alpine.store('colors').score = score;     
+  },
+  
+  setHSL(i) {
+    let hex = Alpine.store('colors')[`hex${i}`];
+    let hsl = hexToHSL(hex);
+    Alpine.store('colors')[`h${i}`] = hsl.h;
+    Alpine.store('colors')[`s${i}`] = hsl.s;
+    Alpine.store('colors')[`l${i}`] = hsl.l;
+  },
+  
+  setHEX(i){
+    let h1 = Alpine.store('colors')[`h${i}`];
+    let s1 = Alpine.store('colors')[`s${i}`];
+    let l1 = Alpine.store('colors')[`l${i}`];
+    Alpine.store('colors')[`hex${i}`] = hslToHex(h1,s1,l1);
+  }
+  
+}));
+
 Alpine.store('colors', {
     score: '',
     hex1: '',
@@ -13,39 +49,6 @@ Alpine.store('colors', {
     s2: 0,
     l2: 80,
 });
-
-/* swapColors = function(){
-  let a = Alpine.store('colors').hex1, b = Alpine.store('colors').hex2;
-  [a, b] = [b, a];
-  console.log(`${a} ${b}`);
-  Alpine.store('colors').hex1 = a;
-  Alpine.store('colors').hex2 = b;
-  setHSL(1);
-  setHSL(2);
-  calculateScore();
-}; */
-
-calculateScore = function() {
-  let hex1 = Alpine.store('colors').hex1;
-  let hex2 = Alpine.store('colors').hex2;
-  let score = APCAcontrast( sRGBtoY( hex1 ), sRGBtoY( hex2 ) );
-  Alpine.store('colors').score = score;     
-};
-
-setHSL = function(i) {
-  let hex = Alpine.store('colors')[`hex${i}`];
-  let hsl = hexToHSL(hex);
-  Alpine.store('colors')[`h${i}`] = hsl.h;
-  Alpine.store('colors')[`s${i}`] = hsl.s;
-  Alpine.store('colors')[`l${i}`] = hsl.l;
-};
-
-setHEX = function(i){
-  let h1 = Alpine.store('colors')[`h${i}`];
-  let s1 = Alpine.store('colors')[`s${i}`];
-  let l1 = Alpine.store('colors')[`l${i}`];
-  Alpine.store('colors')[`hex${i}`] = hslToHex(h1,s1,l1);
-};
 
  
 hslToHex = function(h, s, l) {
